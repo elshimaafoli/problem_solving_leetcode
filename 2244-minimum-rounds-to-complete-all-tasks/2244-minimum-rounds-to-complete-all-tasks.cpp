f22022fcf31b73cb5f4150bc0d529ef89e6d4d96
elshimaafoli <105022323+elshimaafoli@users.noreply.github.com>
2023-01-04 17:33:12 +0200

    Time: 920 ms (5.07%), Space: 104.3 MB (36.98%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        int count=0;
+        map <int,int> nums;
+        for(int i=0;i<tasks.size();i++)
+        {
+            nums[tasks[i]]++;
+        }
+        
+        for(int i=0;i<tasks.size();i++)
+        { 
+            if(nums[tasks[i]]==0) continue;
+            
+            if(nums[tasks[i]]==1) return -1;
+            
+            int freq=nums[tasks[i]],tc=0;
+            cout<<tasks[i]<<' '<<freq<<endl;
+            if(nums[tasks[i]]%3==0)
+            {
+                count+=(nums[tasks[i]]/3);
+                nums[tasks[i]]=0;
+                continue;
+            }
+            else 
+            {
+            tc+=freq/3;
+            freq%=3;
+            if(freq==1)
+                tc++;
+            else if(freq%2==0)
+            {
+                   tc+=freq/2;
+            }
+           
+            count+=tc;
+            }
+            nums[tasks[i]]=0;
+        }
+        return count;
+    }
+};
