eb7514ced49721c19263222c101da04d898b5285
elshimaafoli <105022323+elshimaafoli@users.noreply.github.com>
2023-01-13 02:37:45 +0200

    Time: 11 ms (91.08%), Space: 10.7 MB (47.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int ind1=0,ind2=0;
-       for(int i=0;i<nums.size();i++)
-       {
-           for(int j=i+1;j<nums.size();j++)
-           {
-                if(nums[i]+nums[j]==target)
-                {
-                    ind1=i;
-                    ind2=j;
-                    break;
-                }
-           }
-       }
+        vector <int> two_sum;
+        for(int i=0;i<nums.size();i++)
+            two_sum.push_back(nums[i]);
+        sort(two_sum.begin(),two_sum.end());            
+        int l=0,r=two_sum.size()-1;
+        while(l<r)
+        {
+            if(two_sum[l]+two_sum[r]==target) 
+            {
+               // cout<<l<<' '<<r<<endl;
+                l=two_sum[l];
+                r=two_sum[r];
+               // cout<<l<<' '<<r<<endl;
+                break;
+            }
+            else if(two_sum[l]+two_sum[r]>target)
+                r--;
+            else
+                l++;
+        }
+        int ind1=-1,ind2=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==l&&ind1==-1) {ind1=i; continue;}
+            if(nums[i]==r){ind2=i;}
+        }
        return {ind1,ind2};
     }
 };
